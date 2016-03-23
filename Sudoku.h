ea2ac3f4e4e4334fe40e2bed6ada006bd6a590ec
ShuoHsuanLi <ching93011@gmail.com>
2016-03-23 17:43:40 +0800

    Define the .h file

---
+++ Sudoku.h
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+class Sudoku{
+
+	public:
+		void giveQuestion();//output 81 digits
+		void readIn();//input 81 digit,and Read in Sudoku board
+		void solve();
+		//use the ReadIn data and output slovable/unsolable(if solvable print 1 and the answer
+		void changeNum(int a,int b);//exchange the number a and b in Sudoku board
+		void changeRow(int a,int b);//exchagne the row set a and b
+		//the row set 0 mean row 0~2 
+		void changeCol(int a,int b);
+		//the same concept as changeRow,but now we change the column
+		void rotate(int n);
+		//Rotate the board clockwise 90 degree,and the input n between 0 and 100
+		flip(int n);
+		//when input n==0,flip the board vertically.Otherwise,flip it horizontally.
+		//input 0<=n<=1
+		transform()ing namespace std;
+
+class Sudoku{
+
+        public:
+                void giveQuestion();//output 81 digits
+                void readIn();//input 81 digit,and Read in Sudoku board
+                void solve();
+                //use the ReadIn data and output slovable/unsolable(if solvable print 1 and the answer
+                void changeNum(int a,int b);//exchange the number a and b in Sudo
+
+
+	private:
+
+}
